5ff5e8a423b167a89b9afaef606f7a000978a599
caseyfee <caseyfee3@gmail.com>
2023-02-12 11:56:03 -0800

    pseudo-coded reset of js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Variables for petFinder
-var petFinderAPI = "SsaICgpvhfjFNtYay7GrAmNtBM4fG2uTLpP4EtnMQ0gPkx1OXr";
-// The "access_token" is the token itself. You’ll need to have your system store this as a variable and include it in the header of every API request until it expires and you request another.
-// var access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJTc2FJQ2dwdmhmakZOdFlheTdHckFtTnRCTTRmRzJ1VExwUDRFdG5NUTBnUGt4MU9YciIsImp0aSI6IjdlZDdiMjY2MTNiYmJlMzcyNDdlZGU2ZWE5ZTQ4ZjNlODFiYzIxYWY4OGFlYmZmOGNiOGRlYTYxOGZlMTkwNzhiMDAwOWIxN2JmMTE3YmE4IiwiaWF0IjoxNjc1ODI1Mzg5LCJuYmYiOjE2NzU4MjUzODksImV4cCI6MTY3NTgyODk4OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.pT6ICE9ed2ajGGpHbjP5mNy8UjcQY_1hBZXwHuhSZ2el1G8x2O2ewNOWC5DVHYdcuVHbq4a9XpJ2sJbfCz5E1ooJ_gTNYymdfoJ70clgIezfur1QYzWAuxrVX9e7ZY5OEVC3F1ri2fuVnJf7juRgF5IcNrlElq4C22oGmwx6_t-S6ecGRzhEw1f5dYz9BFyjA96MUs5jy_hZm1f3PTqtzDn89uF_poZ5lXnIh43mMdwmI_h1G6Ya8yR6zvziAioWOHzNXlECumJrTsuv5DrrX5RfcsZOV_wHc9h7dj1E2KGcPmPgtukOX7ClVosnZmlW8FQc5OOT2M-jUpuJfS8lyQ"}%";
-// To pull from PetFinder- use GET requests using the following patterns:
-// curl -H "Authorization: Bearer {YOUR_ACCESS_TOKEN}" GET https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
-// Documentation about what PetFinder API errors mean: https://www.rfc-editor.org/rfc/rfc7807
 
 var bingAPI = "AjyUKW6RaQn4BQSYjKo0uvtRaDumIpGMR_5Eyex2C0lkul8hXnbD05vXh8TVePWi";
 
@@ -20,9 +13,7 @@ var ticketMasterLA = "https://app.ticketmaster.com/discovery/v2/events.json?clas
 // ticket example URL searches with our API key
 // Get a list of all events in the United States https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=rGS5yWSlAMAia16Qiej1YcdN2Y1QXhNi
 // Search for music events in the Los Angeles area https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=rGS5yWSlAMAia16Qiej1YcdN2Y1QXhNi
-
 // Get a list of all events for Adele in Canada https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=CA&apikey=rGS5yWSlAMAia16Qiej1YcdN2Y1QXhNi
-
 // TicketMaster docs: https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
 // Dom variables
 
@@ -30,12 +21,30 @@ var startLat = "41.837285";
 var startLon = "-88.284333";
 
 
+// User inputs starting address - 
+
+    // Start function - onclick button to ssave address and send to TM
+    // Validitation of the address step? Use modal to tell user it didn't work
+    // Add suggestion of how to format address in the search bar
+    // Start with an actual full search (Button)?
+    // Console log 
+
+
+    // Address or coordinates are saved into local storage and send to Bing
+
+    // Clear History Button
+
+
+// Start address is sent to TM to find events within 50 miles (either in address or converted to lat/long)
+
 // Create a function to pull search results off of homepage
 // Pushes to 'Search Page'
 
 var eventListHTML = ``;
 var eventLat = "";
 var eventLon = "";
+var radius = "50"; 
+// may need to be # ^^
 
 fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=rGS5yWSlAMAia16Qiej1YcdN2Y1QXhNi")
     .then(function (data) {
@@ -70,11 +79,21 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=
 
             document.querySelector('#eventList').innerHTML+= eventListHTML;
         } 
+
+        // POTENTIALLY EXTRA TASK If user wants more events, link to the actual TM page AND BING maps
        
-        // var eventsArray = [];
-        // populateEventList(eventResponse);
+        // POTENTIALLY EXTRA TASK Save search results in local storage so it is not lost during refresh
+
+        // Link remove history button after events are listed (id hide)
+
     }) 
 }) 
-// function populateEventList(eventResponse) {
 
+// User chooses an event using the button and event address is sent to Bing
+    // Use modal?
+    // POTENTIALLY EXTRA TASK - add functionality of map (share it with user's phone or something)
+
+
+
+// Bing returns a map with starting and ending address
 
